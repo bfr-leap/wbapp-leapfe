3ecf11d01f7c5439000b13bcf5e9520c3f96de1d
Arturo Mayorga <arturo@mayorga.io>
2022-12-11 14:00:56 -0500

    update derived data

--- scraper/derived-data.ts
+++ scraper/derived-data.ts
@@ -31,47 +31,53 @@ function deriveLeagueSimSessionIndex(leagueId: number) {
 
         indices.push(index);
 
-        let lss = getLeagueSeasonSessions(leagueId, s.season_id);
+        try {
+            let lss = getLeagueSeasonSessions(leagueId, s.season_id);
 
-        for (let se of lss.sessions) {
-            se.subsession_id;
-            let simSessionRetry = 8;
+            for (let se of lss.sessions) {
+                se.subsession_id;
+                let simSessionRetry = 8;
 
-            let session: SSI_Session = {
-                session_id: se.session_id,
-                subsession_id: se.subsession_id,
-                session_title: `${se.track.track_name} - ${se.cars[0].car_name}`,
-                simsessions: [],
-            };
-            index.sessions.push(session);
+                let session: SSI_Session = {
+                    session_id: se.session_id,
+                    subsession_id: se.subsession_id,
+                    session_title: `${se.track.track_name} - ${se.cars[0].car_name}`,
+                    simsessions: [],
+                };
+                index.sessions.push(session);
 
-            for (let i = 0; i < simSessionRetry; ++i) {
-                try {
-                    let cd = getLapChartData(se.subsession_id, -1 * i);
-                    let simtype = cd.session_info.simsession_type;
-                    let simsession: SSI_Simsession = {
-                        simsession_id: -1 * i,
-                        type:
-                            simtype === 6
-                                ? 'race'
-                                : simtype === 5
-                                ? 'qualify'
-                                : 'practice',
-                    };
+                for (let i = 0; i < simSessionRetry; ++i) {
+                    try {
+                        let cd = getLapChartData(se.subsession_id, -1 * i);
+                        let simtype = cd.session_info.simsession_type;
+                        let simsession: SSI_Simsession = {
+                            simsession_id: -1 * i,
+                            type:
+                                simtype === 6
+                                    ? 'race'
+                                    : simtype === 5
+                                    ? 'qualify'
+                                    : 'practice',
+                        };
 
-                    if (simsession.type === 'race') {
-                        let lapCount = cd.chunk_info
-                            .map((c) => c.lap_number)
-                            .reduce((p, c) => Math.max(p, c), 0);
+                        if (simsession.type === 'race') {
+                            let lapCount = cd.chunk_info
+                                .map((c) => c.lap_number)
+                                .reduce((p, c) => Math.max(p, c), 0);
 
-                        if (lapCount < 10) {
-                            simsession.type = 'sprint';
+                            if (lapCount < 10) {
+                                simsession.type = 'sprint';
+                            }
                         }
-                    }
 
-                    session.simsessions.push(simsession);
-                } catch (e) {}
+                        session.simsessions.push(simsession);
+                    } catch (e) {}
+                }
             }
+        } catch (e) {
+            console.log(
+                `error fetching info for season [${s.season_name}: ${s.season_id}]`
+            );
         }
     }
 
