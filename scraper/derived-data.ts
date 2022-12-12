4107c3a9294b8e5329b63b5c817821e69e74080a
Arturo Mayorga <arturo@mayorga.io>
2022-12-11 21:46:39 -0500

    first pass at driver stats

--- scraper/derived-data.ts
+++ scraper/derived-data.ts
@@ -77,6 +77,115 @@ function acceptLapChartDataVisitor(
     }
 }
 
+type DriverStatsMap = { [name: number]: DriverStats };
+
+interface DriverStats {
+    cust_id: number;
+    started: number;
+    finished: number;
+    wins: number;
+    podiums: number;
+    top_10: number;
+    top_20: number;
+    fast_laps: number;
+    hard_charger: number;
+    poles: number;
+    power_points: number;
+}
+
+function getStats(map: DriverStatsMap, custId: number): DriverStats {
+    let ret = map[custId];
+    if (!ret) {
+        ret = map[custId] = {
+            cust_id: custId,
+            started: 0,
+            finished: 0,
+            wins: 0,
+            podiums: 0,
+            top_10: 0,
+            top_20: 0,
+            fast_laps: 0,
+            hard_charger: 0,
+            poles: 0,
+            power_points: 0,
+        };
+    }
+
+    return ret;
+}
+
+function getSeasonStatsMap(
+    map: { [name: number]: DriverStatsMap },
+    seasonId: number
+): DriverStatsMap {
+    let ret = map[seasonId];
+    if (!ret) {
+        ret = map[seasonId] = {};
+    }
+    return ret;
+}
+
+function deriveDriverStats(leagueId: number) {
+    let carrerStatsMap: DriverStatsMap = {};
+    let seasonStatsMapStore: { [name: number]: DriverStatsMap } = {};
+
+    seasonStatsMapStore[0] = carrerStatsMap;
+
+    acceptLapChartDataVisitor(
+        leagueId,
+        (
+            leagueSeasons: LeagueSeasons,
+            seasonInfo: LS_SeasonSummary,
+            leaguSeasonSessions: LeagueSeasonSessions,
+            sessionInfo: LSS_Session,
+            lapChartData: LapChartData
+        ) => {
+            let r: SimsessionResults;
+            let seasonStatsMap = getSeasonStatsMap(
+                seasonStatsMapStore,
+                seasonInfo.season_id
+            );
+            if (lapChartData.session_info.simsession_type === 6) {
+                r = calculateRaceResults(lapChartData);
+
+                for (let res of r.results) {
+                    let cStats = getStats(carrerStatsMap, res.cust_id);
+                    let sStats = getStats(seasonStatsMap, res.cust_id);
+                    cStats.started += 1;
+
+                    if (res.position === 1) {
+                        cStats.wins += 1;
+                        sStats.wins += 1;
+                    }
+
+                    if (res.position <= 3) {
+                        cStats.podiums += 1;
+                        sStats.podiums += 1;
+                    }
+
+                    if (res.position <= 10) {
+                        cStats.top_10 += 1;
+                        sStats.top_10 += 1;
+                    }
+
+                    if (res.position <= 20) {
+                        cStats.top_20 += 1;
+                        sStats.top_20 += 1;
+                    }
+                }
+            } else {
+                r = calculateQualifyResults(lapChartData);
+                let cStats = getStats(carrerStatsMap, r.results[0].cust_id);
+                let sStats = getStats(seasonStatsMap, r.results[0].cust_id);
+                cStats.poles += 1;
+                sStats.poles += 1;
+            }
+        }
+    );
+
+    wf(seasonStatsMapStore, `leagueDriverStats_${leagueId}.json`);
+}
+
 function deriveLeagueSimSessionResults(leagueId: number) {
     acceptLapChartDataVisitor(
         leagueId,
@@ -172,4 +281,5 @@ function deriveLeagueSimSessionIndex(leagueId: number) {
 }
 
 //deriveLeagueSimSessionIndex(6555);
-deriveLeagueSimSessionResults(6555);
+// deriveLeagueSimSessionResults(6555);
+deriveDriverStats(6555);
