401c64d5e1af84a770634e822c62a718d4d401b9
Arturo Mayorga <arturo@mayorga.io>
2022-12-15 01:41:43 -0500

    adding summary for each season to user profile

--- src/driver-profile.ts
+++ src/driver-profile.ts
@@ -7,45 +7,124 @@ import type {
     DriverStatsMap,
     DriverStats,
     M_Member,
+    LeagueSeasons,
 } from './iracing-endpoints';
 
 const INCLUDE_IDS = false;
 
+async function fetchObjects(urls: string[]): Promise<any[]> {
+    let objs = await Promise.all(
+        (
+            await Promise.all(urls.map((url) => fetch(url)))
+        ).map((response) => response.json())
+    );
+
+    return objs;
+}
+
 export class DriverProfile {
     private _teamsInfo: CuratedLeagueTeamsInfo | null = null;
+    private _driverStatsMap: { [name: number]: DriverStatsMap } | null = null;
 
     public constructor(leagueId: string, custId: string) {
         let element = document.createElement('div');
         element.className = 'card-container';
-        element.innerHTML = `<div class='card-item' id="driver-title-card"></div>`;
+        element.innerHTML = `<div class='card-item' id="driver-title-card"></div><div class='card-item' id="driver-seasons-card"></div>`;
         document.body.appendChild(element);
 
-        fetch(`./data/derived/leagueDriverStats_${leagueId}.json`)
-            .then((response) => {
-                return response.json();
-            })
-            .then((driverStatsMap: DriverStatsMap) => {
-                console.log(driverStatsMap);
-                fetch(`./data/curated/leagueTeamsInfo_${leagueId}.json`)
-                    .then((response) => {
-                        return response.json();
-                    })
-                    .then((leagueTeamsInfo: CuratedLeagueTeamsInfo) => {
-                        console.log(leagueTeamsInfo);
-                        fetch(`./data/derived/singleMemberData_${custId}.json`)
-                            .then((response) => {
-                                return response.json();
-                            })
-                            .then((singleMemberData: M_Member) => {
-                                console.log(singleMemberData);
-                                this._teamsInfo = leagueTeamsInfo;
-                                this.renderTitle(
-                                    singleMemberData,
-                                    leagueTeamsInfo
-                                );
-                            });
-                    });
-            });
+        (async () => {
+            let [
+                driverStatsMap,
+                leagueTeamsInfo,
+                singleMemberData,
+                leagueSeasons,
+            ] = <
+                [
+                    { [name: number]: DriverStatsMap },
+                    CuratedLeagueTeamsInfo,
+                    M_Member,
+                    LeagueSeasons
+                ]
+            >await fetchObjects([
+                `./data/derived/leagueDriverStats_${leagueId}.json`,
+                `./data/curated/leagueTeamsInfo_${leagueId}.json`,
+                `./data/derived/singleMemberData_${custId}.json`,
+                `./data/scraped/leagueSeasons_${leagueId}.json`,
+            ]);
+
+            this._driverStatsMap = driverStatsMap;
+            this._teamsInfo = leagueTeamsInfo;
+            this.renderTitle(singleMemberData, leagueTeamsInfo);
+
+            this.renderSeasons(Number.parseInt(custId), leagueSeasons);
+        })();
+    }
+
+    private renderSeasons(custId: number, leagueSeasons: LeagueSeasons) {
+        let parentDiv = document.getElementById('driver-seasons-card');
+
+        for (let season of leagueSeasons.seasons) {
+            let seasonId = season.season_id;
+            let m = this._driverStatsMap[seasonId];
+            if (!m) {
+                continue;
+            }
+
+            let stats = m[custId];
+
+            if (!stats) {
+                continue;
+            }
+
+            this.renderStats(
+                parentDiv,
+                stats,
+                season.season_name,
+                season.season_id
+            );
+        }
+
+        let m = this._driverStatsMap[0];
+        if (!m) {
+            return;
+        }
+
+        let stats = m[custId];
+
+        if (!stats) {
+            return;
+        }
+
+        this.renderStats(parentDiv, stats, 'All Time');
+    }
+
+    private renderStats(
+        parentDiv: HTMLElement,
+        stats: DriverStats,
+        seasonName: string,
+        seasonId: number = 0
+    ) {
+        let newNode = document.createElement('div');
+        let seasonTag = seasonId === 0 ? 'span' : 'a';
+        let optionalHref =
+            seasonId === 0 ? '' : ` href="?&m=user-index&season=${seasonId}"`;
+        newNode.className = 'linkbtn-item';
+        newNode.innerHTML = `
+                    <div class='linkbtn-item linkbtn-fullrow'><${seasonTag}${optionalHref}>${seasonName}</${seasonTag}></div>
+                    <div class='driver-stat'><span class='name'>Starts:</span><span class='value'> ${stats.started}</span></div>
+                    <div class='driver-stat'><span class='name'>Poles:</span><span class='value'> ${stats.poles}</span></div>
+                    <div class='driver-stat'><span class='name'>Wins:</span><span class='value'> ${stats.wins}</span></div>
+                    <div class='driver-stat'><span class='name'>Podiums:</span><span class='value'> ${stats.podiums}</span></div>
+                    <div class='driver-stat'><span class='name'>Top 10:</span><span class='value'> ${stats.top_10}</span></div>
+                    <div class='driver-stat'><span class='name'>Top 20:</span><span class='value'> ${stats.top_20}</span></div>
+                    <div class='driver-stat'><span class='name'>Power Points:</span><span class='value'> ${stats.power_points}</span></div>
+                    <div class='linkbtn-item linkbtn-fullrow'></div>`;
+
+        // todo:
+        // finished: number;
+        // fast_laps: number;
+        // hard_charger: number;
+        parentDiv.appendChild(newNode);
     }
 
     private renderTitle(
@@ -73,7 +152,7 @@ export class DriverProfile {
             Math.floor((irating % 1000) / 100) +
             'k';
 
-        let teamName = 'Privateer';
+        let teamName = '';
 
         let parentDiv = document.getElementById('driver-title-card');
 
