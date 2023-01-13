4b1d833aa69d8714a91dde72cbb4126fe8b1c0b2
Arturo Mayorga <arturo@mayorga.io>
2023-01-12 23:57:21 -0500

    grouping the charts and tables in the driver profile into tabs

--- src/components/Stats.vue
+++ src/components/Stats.vue
@@ -109,29 +109,82 @@ const statClasses = 'px-2 py-1 m-1 fs-5';
             ><span class="value"> {{ stats.power_points }}</span>
         </div>
     </div>
-    <div v-if="barChartData" class="row">
-        <div class="d-flex flex-wrap">Qualifying Performance:</div>
-    </div>
-    <div class="row">
-        <div class="col-12 m-auto">
-            <BarChart v-if="barChartData" :data="barChartData" />
-        </div>
-    </div>
 
-    <template v-for="(result, i) in results">
+    <ul class="nav nav-pills">
+        <li v-if="barChartData" class="nav-item">
+            <a
+                class="nav-link active"
+                data-bs-toggle="tab"
+                v-bind:data-bs-target="`#nav-qpchart-${seasonId}`"
+                aria-current="page"
+                href="#"
+                >Qualifying Performance</a
+            >
+        </li>
+        <li v-if="results.length > 0" class="nav-item dropdown">
+            <a
+                class="nav-link dropdown-toggle"
+                data-bs-toggle="dropdown"
+                href="#"
+                role="button"
+                aria-expanded="false"
+                >Tables</a
+            >
+            <ul class="dropdown-menu">
+                <template v-for="(result, i) in results">
+                    <li v-if="result && result[seasonId]">
+                        <a
+                            class="dropdown-item"
+                            data-bs-toggle="tab"
+                            v-bind:data-bs-target="`#nav-${
+                                ['race', 'sprint', 'qualy'][i]
+                            }-${seasonId}`"
+                            href="#"
+                        >
+                            {{ ['Race', 'Sprint', 'Qualy'][i] }}
+                        </a>
+                    </li>
+                </template>
+            </ul>
+        </li>
+    </ul>
+    <div class="tab-content" id="nav-tabContent">
         <div
-            class="row"
-            style="margin-top: 3em"
-            v-if="result && result[seasonId]"
+            class="tab-pane fade show active"
+            v-bind:id="`nav-qpchart-${seasonId}`"
+            role="tabpanel"
+            aria-labelledby="nav-home-tab"
+            tabindex="0"
         >
-            <div class="col">{{ ['Race', 'Sprint', 'Qualy'][i] }}</div>
+            <div class="row">
+                <div class="col-12 m-auto">
+                    <BarChart v-if="barChartData" :data="barChartData" />
+                </div>
+            </div>
         </div>
-        <ResultsTable
-            :seasonId="seasonId"
-            :results="result"
-            v-bind:leagueId="props.leagueId"
-        />
-    </template>
+        <template v-for="(result, i) in results">
+            <div
+                class="tab-pane fade"
+                v-bind:id="`nav-${['race', 'sprint', 'qualy'][i]}-${seasonId}`"
+                role="tabpanel"
+                aria-labelledby="nav-profile-tab"
+                tabindex="0"
+            >
+                <div
+                    class="row"
+                    style="margin-top: 1em"
+                    v-if="result && result[seasonId]"
+                >
+                    <div class="col">{{ ['Race', 'Sprint', 'Qualy'][i] }}</div>
+                </div>
+                <ResultsTable
+                    :seasonId="seasonId"
+                    :results="result"
+                    v-bind:leagueId="props.leagueId"
+                />
+            </div>
+        </template>
+    </div>
 
     <div class="linkbtn-item linkbtn-fullrow"></div>
 </template>
