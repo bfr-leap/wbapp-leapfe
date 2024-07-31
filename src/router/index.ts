454425eca67e9e19e84e5053310d1948d40d0031
unknown <arturo.mayorga@protonmail.com>
2024-07-31 00:16:08 -0400

    improve signin funnel

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import SignInRedirect from '../views/SignInRedirect.vue';
+import { useAuth } from 'vue-clerk';
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +11,31 @@ const router = createRouter({
             name: 'home',
             component: HomeView,
         },
+        {
+            path: '/sign-in',
+            name: 'signin',
+            component: SignInRedirect,
+        },
     ],
+});
+
+router.beforeEach(async (to, from, next) => {
+    let { isSignedIn } = useAuth();
+
+    const publicPages = ['/sign-in'];
+    const authRequired = !publicPages.includes(to.path);
+
+    console.log(isSignedIn.value);
+    if (isSignedIn.value === false && authRequired) {
+        localStorage.setItem('redirect-target', to.fullPath);
+        next('/sign-in');
+    } else {
+        if (isSignedIn.value === true && to.path === '/sign-in') {
+            next('/');
+        } else {
+            next();
+        }
+    }
 });
 
 export default router;
